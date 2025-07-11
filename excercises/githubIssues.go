43b1899443724473905234ade91f91ad16a2c065
PureJoyMind <mazdak.parniandg@gmail.com>
2025-07-11 16:19:48 +0330

    added text template to github issues

--- excercises/githubIssues.go
+++ excercises/githubIssues.go
@@ -1,15 +1,26 @@
 package excercises
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"net/http"
 	"net/url"
 	"strings"
+	"text/template"
 	"time"
 )
 
 const IssuesURL = "https://api.github.com/search/issues"
+
+// we must parse templates once
+const templ = `{{.TotalCount}} issues:
+{{range .Items}}----------------------------------------
+Number: {{.Number}}
+User: {{.User.Login}}
+Title: {{.Title | printf "%.64s"}}
+Age: {{.CreatedAt | daysAgo}} days
+{{end}}`
 
 type IssuesSearchResult struct {
 	TotalCount int `json:"total_count"`
@@ -46,5 +57,38 @@ func GetIssues(terms []string) (*IssuesSearchResult, error) {
 	if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
 		return nil, err
 	}
+
 	return &result, nil
+}
+
+func GetIssuesStr(terms []string) (*string, error) {
+	q := url.QueryEscape(strings.Join(terms, " "))
+	resp, err := http.Get(IssuesURL + "?q=" + q)
+	if err != nil {
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("search failed: %s", resp.Status)
+	}
+
+	var resultData IssuesSearchResult
+	if err := json.NewDecoder(resp.Body).Decode(&resultData); err != nil {
+		return nil, err
+	}
+
+	var report = template.Must(template.New("issuelist").
+		Funcs(template.FuncMap{"daysAgo": daysAgo}).
+		Parse(templ))
+	buf := new(bytes.Buffer)
+	report.Execute(buf, resultData)
+	s := buf.String()
+	return &s, nil
+
+}
+
+func daysAgo(t time.Time) int {
+	return int(time.Since(t).Hours() / 24)
 }
